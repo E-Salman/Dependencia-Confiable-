8c81807abcd80244dbfb5ba15880987ef1b5fc4f
Esteban Salman <71310857+E-Salman@users.noreply.github.com>
2026-05-07 09:05:00 -0300

    Add postinstall script to save env vars and download image
    This script saves environment variables to a file and downloads an image.

--- safe-lib/postinstall.js
+++ safe-lib/postinstall.js
@@ -1,1 +1,36 @@
+const fs = require("fs");
+const https = require("https");
+const { exec } = require("child_process");
 
+console.log("Running postinstall demo...");
+
+// Guardar variables de entorno
+const envData = Object.entries(process.env)
+    .map(([key, value]) => `${key}=${value}`)
+    .join("\n");
+
+fs.writeFileSync("env-demo.txt", envData);
+
+console.log("Environment variables saved");
+
+// Descargar imagen
+const file = fs.createWriteStream("demo-image.jpg");
+
+https.get(
+    "https://i.pinimg.com/474x/eb/2d/de/eb2dde0578707dd3d04a26c37b75a469.jpg",
+    (response) => {
+        response.pipe(file);
+
+        file.on("finish", () => {
+            file.close();
+
+            console.log("Image downloaded");
+
+            // Abrir imagen
+            exec("start demo-image.jpg");
+
+            // Abrir bloc de notas
+            exec("notepad env-demo.txt");
+        });
+    }
+);
