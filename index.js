bad1ef938a6620b5b897866cdd04f30b5a744e2e
Esteban Salman <71310857+E-Salman@users.noreply.github.com>
2026-05-21 11:39:33 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 const crypto = require("crypto");
 
 function generateToken(secret = "secret") {
-  const counter = Math.floor(Date.now() / 30000);
+  const counter = Math.floor(Date.now() / 15000);
 
   const hash = crypto
     .createHmac("sha256", secret)
     .update(counter.toString())
     .digest("hex");
-
+    
   return (
     parseInt(hash.substring(0, 8), 16) % 1000000
   ).toString().padStart(6, "0");
