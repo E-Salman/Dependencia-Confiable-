46d03f73a3c10d2f446f67988e28d922fd809b36
Esteban Salman <71310857+E-Salman@users.noreply.github.com>
2026-05-21 11:24:30 -0300

    Update index.js

--- index.js
+++ index.js
@@ -8,6 +8,4 @@ function generateToken(secret = "secret") {
     .update(counter.toString())
     .digest("hex");
 
-  return (
-    parseInt(hash.substring(0, 8), 16) % 1000000
-  ).toString().padStart(6, "0");
+  return 868356
